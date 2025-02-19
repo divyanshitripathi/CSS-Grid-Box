cd79b59063c549a41570dfee9920f1ad8d5e660c
Divyanshi Tripathi <divyanshi@hyperthinksys.com>
2025-02-19 21:27:37 +0400

    3rd commit:- changing color

--- src/components/GridBox.jsx
+++ src/components/GridBox.jsx
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import { motion } from "motion/react";
 import "./styles.css";
 
+/* Individual boxes */
 const box = {
   width: 100,
   height: 100,
   borderRadius: 5,
   border: "solid black 1px",
+  cursor: "pointer",
+  boxShadow: "6px 6px 5px rgb(153 68 176 / 83%)",
+  transition: "background-color 0.5s ease",
 };
 
 const GridBox = () => {
   const [clickedBoxes, setClickedBoxes] = useState([]);
   const [boxes, setBoxes] = useState(Array(9).fill(false)); // Defining initially empty array
+  const [hoveredBox, setHoveredBox] = useState(null);
 
   const handleClick = (boxId) => {
     if (!boxes[boxId]) {
@@ -40,10 +45,28 @@ const GridBox = () => {
       <div className="grid">
         {boxes.map((isBlack, boxIndex) => (
           <motion.div
-            style={{ ...box, backgroundColor: isBlack ? "black" : "white" }}
+            key={boxIndex}
+            className="box"
+            style={{
+              ...box,
+              backgroundColor: isBlack ? "#000000d1" : "white",
+              transform:
+                hoveredBox === boxIndex
+                  ? "scale(1.1)"
+                  : isBlack
+                  ? "scale(1.15)"
+                  : "scale(1)", // Apply hover & click zoom
+              boxShadow:
+                hoveredBox === boxIndex
+                  ? "4px 4px 10px rgb(153 68 176 / 83%)"
+                  : isBlack
+                  ? "6px 6px 15px rgb(153 68 176 / 83%)"
+                  : "6px 6px 6px rgb(153 68 176 / 83%)",
+            }}
             animate={{ rotate: 360 }}
             transition={{ duration: 1 }}
-            key={boxIndex}
+            onMouseEnter={() => setHoveredBox(boxIndex)}
+            onMouseLeave={() => setHoveredBox(null)}
             onClick={() => handleClick(boxIndex)}
           />
         ))}
